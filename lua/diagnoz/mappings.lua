e0c7bee0a8b206c196d12607cdc8b298666324ec
RAprogramm <andrey.rozanov.vl@gmail.com>
2024-07-13 16:15:27 +0900

    which-key v3

--- lua/diagnoz/mappings.lua
+++ lua/diagnoz/mappings.lua
@@ -2,55 +2,55 @@ local wk = require('which-key')
 local icon = require('stigmata.utils').icon
 
 return function(client, bufnr)
-  wk.register({
-    ['<leader>'] = {
-      d = {
-        name = 'Diagnostics' .. icon('', 'pulse', 1),
-        w = {
-          ':Telescope diagnostics severity_bound=ERROR<cr>',
-          'Workspace diagnostics',
-        },
-        l = {
-          function()
-            vim.diagnostic.open_float(nil, {
-              focusable = true,
-              border = vim.g.b,
-            })
-          end,
-          'Show diagnostic on line',
-        },
-        p = {
-          ':lua vim.diagnostic.goto_prev({float=false})<cr>',
-          'Jump to previous diagnostic node',
-        },
-        n = {
-          ':lua vim.diagnostic.goto_next({float=false})<cr>',
-          'Jump to next diagnostic node',
-        },
-        -- TODO: move to debugging module
-        d = {
-          name = 'DAP',
-          b = { ':DapToggleBreakpoint<cr>', 'Toggle breakpoint' },
-          r = { ':DapContinue<cr>', 'Run debug' },
-          t = { ':DapTerminate<cr>', 'Terminate DAP' },
-          s = {
-            name = 'steps',
-            O = { ':DapStepOut<cr>', 'Out' },
-            o = { ':DapStepOver<cr>', 'Over' },
-            i = { ':DapStepInto<cr>', 'Into' },
-          },
-          L = {
-            name = 'Set log level ',
-            w = { ':DapSetLogLevel WARN<cr>', 'Warning' },
-            i = { ':DapSetLogLevel INFO<cr>', 'Information' },
-            d = { ':DapSetLogLevel DEBUG<cr>', 'Debug' },
-            e = { ':DapSetLogLevel ERROR<cr>', 'Error' },
-            t = { ':DapSetLogLevel TRACE<cr>', 'Trace' },
-          },
-          l = { ':DapShowLog<cr>', 'Show log' },
-          R = { ':DapToggleRepl<cr>', 'Toggle REPL' },
-        },
-      },
+  wk.add({
+    buffer = bufnr,
+    mode = { 'n' },
+    { '<leader>d', group = 'Diagnostics', icon = icon('', 'pulse') },
+    {
+      '<leader>dw',
+      '<cmd>Telescope diagnostics severity_bound=ERROR<cr>',
+      desc = 'Workspace diagnostics',
     },
-  }, { buffer = bufnr, mode = 'n' })
+    {
+      '<leader>dl',
+      rhs = function()
+        vim.diagnostic.open_float(nil, {
+          focusable = true,
+          border = vim.g.b,
+        })
+      end,
+      desc = 'Show diagnostic on line',
+    },
+    {
+      '<leader>dp',
+      '<cmd>lua vim.diagnostic.goto_prev({float=false})<cr>',
+      desc = 'Jump to previous diagnostic node',
+    },
+    {
+      '<leader>dn',
+      '<cmd>lua vim.diagnostic.goto_next({float=false})<cr>',
+      desc = 'Jump to next diagnostic node',
+    },
+    -- TODO: move to debugging module
+    { '<leader>dd', group = 'DAP' },
+    {
+      '<leader>ddb',
+      '<cmd>DapToggleBreakpoint<cr>',
+      desc = 'Toggle breakpoint',
+    },
+    { '<leader>ddr', '<cmd>DapContinue<cr>', desc = 'Run debug' },
+    { '<leader>ddt', '<cmd>DapTerminate<cr>', desc = 'Terminate DAP' },
+    { '<leader>dds', group = 'steps' },
+    { '<leader>ddsO', '<cmd>DapStepOut<cr>', desc = 'Out' },
+    { '<leader>ddso', '<cmd>DapStepOver<cr>', desc = 'Over' },
+    { '<leader>ddsi', '<cmd>DapStepInto<cr>', desc = 'Into' },
+    { '<leader>ddL', group = 'Set log level ' },
+    { '<leader>ddLw', '<cmd>DapSetLogLevel WARN<cr>', desc = 'Warning' },
+    { '<leader>ddLi', '<cmd>DapSetLogLevel INFO<cr>', desc = 'Information' },
+    { '<leader>ddLd', '<cmd>DapSetLogLevel DEBUG<cr>', desc = 'Debug' },
+    { '<leader>ddLe', '<cmd>DapSetLogLevel ERROR<cr>', desc = 'Error' },
+    { '<leader>ddLt', '<cmd>DapSetLogLevel TRACE<cr>', desc = 'Trace' },
+    { '<leader>ddl', '<cmd>DapShowLog<cr>', desc = 'Show log' },
+    { '<leader>ddR', '<cmd>DapToggleRepl<cr>', desc = 'Toggle REPL' },
+  })
 end
